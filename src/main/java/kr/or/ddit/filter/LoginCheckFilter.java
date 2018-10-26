db8aca43d53ab137218bac3a50e9dce4d4e7935b
najinsil <sally1334@naver.com>
2018-10-26 12:10:45 +0900

    LoginCheckFilter

--- src/main/java/kr/or/ddit/filter/LoginCheckFilter.java
+++ src/main/java/kr/or/ddit/filter/LoginCheckFilter.java
@@ -1,42 +1,49 @@
 package kr.or.ddit.filter;
-
-import java.io.IOException;
-
-import javax.servlet.Filter;
+ import java.io.IOException;
+ import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-
-
-@WebFilter("/loginCheckFilter")
-public class LoginCheckFilter implements Filter {
-
-  
-	public void destroy() {
-		
+ public class LoginCheckFilter implements Filter{
+ 	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {	
 	}
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		chain.doFilter(request, response);
+ 	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpSession session = req.getSession();
 		
-		HttpSession session = ((HttpServletRequest) request).getSession();
-	/*	
-		session.getAttribute("S_USER");
+		String uri = req.getRequestURI();
 		
-		if(session!=null){
-			//request.getRequestDispatcher("/login/login.jsp").forward(request, response);
-			System.out.println("세션에 사용자 정보 없음.");
+		System.out.println("uri : " + uri);
+		
+		//세션 없이 접속 가능한 요청(로그인 페이지, 로그인 요청)
+		if(uri.equals("/") || 
+		  uri.equals("/login/login.jsp") || 
+		  uri.equals("/dditLogin")||
+		  uri.startsWith("/css") ||
+		  uri.startsWith("/js")){
+			
+			System.out.println("access allowed");
+			chain.doFilter(request, response);
 		}
-		*/
+		else{
+			if(session.getAttribute("S_USER") == null){
+				System.out.println("access not allowed");
+				request.getRequestDispatcher("/").forward(request, response);
+			}
+			else{
+				System.out.println("access allowed");
+				chain.doFilter(request, response);
+			}		
+		}	
 	}
-
-	public void init(FilterConfig fConfig) throws ServletException {
-		
+ 	@Override
+	public void destroy() {
 	}
-
-}
+ }
